bad5c208490c7c9ebcf81b55bec77202ff2bc6e8
Ily@s <ilias.ihsanov@gmail.com>
2019-02-09 15:19:54 +0300

    Initial Commit

---
+++ NewsParser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsParser
+//
+//  Created by Ильяс Ихсанов on 09/02/2019.
+//  Copyright © 2019 ilyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
